197717073f676778023206a011e2e4dc33e51564
Paul Asjes <paulasjes@stripe.com>
2023-06-15 18:37:36 +0200

    here you go domi

--- server.ts
+++ server.ts
@@ -10,4 +10,19 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 // Create new express app
 const app: express.Application = express();
 
+app.get('/payment_intent', async (req: express.Request, res: express.Response) => {
+  const customer: Stripe.Customer = await stripe.customers.create();
+
+  const paymentIntent: Stripe.PaymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: 'eur',
+    customer: customer.id,
+  });
+
+  return res.json({
+    paymentIntent: paymentIntent.client_secret,
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
 app.listen(3000, () => console.log('Running on port 3000'));
